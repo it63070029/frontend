5d2f256ac6730265673ec3477d1f6877e216fe1f
Kotchakorn <73680019+Kotchakorn63070001@users.noreply.github.com>
2022-12-17 23:06:11 +0700

    update delete car

--- src/views/OurCar.vue
+++ src/views/OurCar.vue
@@ -30,7 +30,7 @@
                                     <font-awesome-icon icon="fa-solid fa-marker" />
                                 </span>
                             </button>
-                            <button class="button is-small is-danger is-light is-rounded" aria-haspopup="true" aria-controls="dropdown-menu3" style="padding-left: 1em; padding-right: 1em;" @click="delCar(car.model)">
+                            <button class="button is-small is-danger is-light is-rounded" aria-haspopup="true" aria-controls="dropdown-menu3" style="padding-left: 1em; padding-right: 1em;" @click="delCarConfirm(car.model)">
                                 <span class="icon is-medium">
                                     <font-awesome-icon icon="fa-solid fa-trash" />
                                 </span>
@@ -43,8 +43,28 @@
                 <button class="button is-link">Add New Car</button>
             </div>
         </div>
+        <div class="modal" v-bind:class="{'is-active' : modalDeleteCar}">
+            <div class="modal-background" @click="modalDeleteCar = !modalDeleteCar"></div>
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">Delete Car</p>
+                    <button class="delete" aria-label="close" @click="modalDeleteCar = !modalDeleteCar"></button>
+                </header>
+                <section class="modal-card-body">
+                    <div class="column">
+                        <p>Are you sure you want to delete car model : {{modelDel}}</p>
+                    </div>
+                </section>
+                <footer class="modal-card-foot">
+                    <button class="button is-danger" @click="delCar(modelDel)">Delete</button>
+                    <button class="button" @click="modalDeleteCar = !modalDeleteCar">Cancel</button>
+                </footer>
+            </div>
+        </div>
     </div>
+
     
+
 
 </template>
 
@@ -55,7 +75,8 @@ export default {
     data(){
         return{
             cars: [],
-            test: true
+            modalDeleteCar: false,
+            modelDel: '',
         }
     },
     mounted(){
@@ -71,14 +92,21 @@ export default {
                 console.log(err)
             })
         },
+        delCarConfirm(carModel){
+            this.modalDeleteCar = true
+            console.log(carModel)
+            this.modelDel = carModel
+        },
         delCar(carModel){
             console.log(carModel)
             axios.delete("/cars/delCar/"+carModel)
             .then((res) => {
                 console.log(res)
+                this.modalDeleteCar = false
             })
             .catch((err) => {
                 console.log(err)
+                this.modalDeleteCar = false
             })
         }
     }
